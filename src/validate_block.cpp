0d556076dae94664dbe8624e3dc2099bab600a3c
Daniel Pfeifer <daniel@pfeifer-mail.de>
2026-07-22 17:57:42 +0200

    validate header and block against chain

--- src/validate_block.cpp
+++ src/validate_block.cpp
@@ -2,6 +2,8 @@
 
 module;
 
+#include <ranges>
+
 #include "validation.h"
 
 module bitcoin;
@@ -11,16 +13,29 @@ namespace bitcoin {
 
 validation_status verifier::verify(bitcoin::block const& b) const
 {
-  auto const block = legacy::convert_block(b);
-  auto const params = legacy::convert_consensus(*_params);
+  if (auto status = verify(b.header()); !status.ok()) {
+    return status;
+  }
+
   auto state = BlockValidationState{};
-  bool const result = CheckBlock(block, state, params);
+  bool const result = CheckBlock(legacy::convert_block(b), state,
+                                 legacy::convert_consensus(*_params));
   return result ? 0 : -1; // state.GetResult();
 }
 
-// validation_status verify(bitcoin::block const& block, any_chain_view chain,
-//                            std::chrono::sys_seconds now)
-// {
-// }
+validation_status verifier::verify(bitcoin::block const& b,
+                                   any_chain_view chain, // NOLINT
+                                   std::chrono::sys_seconds now) const
+{
+  if (auto status = verify(b.header(), chain, now); !status.ok()) {
+    return status;
+  }
+
+  auto state = BlockValidationState{};
+  bool const result = ContextualCheckBlock(
+    legacy::convert_block(b), state, legacy::convert_consensus(*_params),
+    AnyChainView{chain | std::views::transform(legacy::convert_header)});
+  return result ? 0 : -1; // state.GetResult();
+}
 
 } // namespace bitcoin
--- src/validate_header.cpp
+++ src/validate_header.cpp
@@ -2,78 +2,47 @@
 
 module;
 
-#include <algorithm>
 #include <chrono>
 #include <ranges>
-#include <vector>
 
-#include "pow.h"
+#include "validation.h"
 
 module bitcoin;
 import legacy;
 
 namespace bitcoin {
-namespace {
-
-auto median_time_past(chain_view auto chain)
-{
-  assert(!chain.empty());
-  auto times = chain
-    | std::views::transform(&block_header::time)
-    | std::views::reverse
-    | std::views::take(11)
-    | std::ranges::to<std::vector>();
-  auto const middle = times.begin() + (times.size() / 2);
-  std::ranges::nth_element(times, middle);
-  return *middle;
-}
-
-} // namespace
 
 validation_status verifier::verify(block_header const& header) const
 {
-  auto const hash = legacy::convert_uint256(block_hash{header});
-  auto const params = legacy::convert_consensus(*_params);
-  bool const result = CheckProofOfWork(hash, header.bits, params);
-  return result ? 0 : -1; // BlockValidationResult::BLOCK_INVALID_HEADER
+  auto state = BlockValidationState{};
+  bool const result = CheckBlockHeader(legacy::convert_header(header), state,
+                                       legacy::convert_consensus(*_params));
+  return result ? 0 : -1; // state.GetResult();
 }
 
 validation_status verifier::verify(block_header const& header,
                                    any_chain_view chain,
                                    std::chrono::sys_seconds now) const
 {
+  // if (auto status = verify(header); !status.ok()) {
+  //   return status;
+  // }
+
   if (chain.empty()) {
     return -1;
   }
 
-  // A header MUST reference the hash of a valid parent block.
-  if (header.prev_block_hash != block_hash{chain[chain.size() - 1]}) {
+  if (header.prev_block_hash
+      != block_hash{std::ranges::common_view(chain).back()}) {
     return -1;
   }
 
-  // A header's hash MUST achieve its own proof-of-work target.
-  // TODO
-
-  // A header's proof-of-work target MUST satisfy the difficulty adjustment
-  // TODO
-
-  // A header timestamp MUST be strictly greater than the median of its 11
-  // ancestors' timestamps.
-  if (header.time <= median_time_past(chain)) {
-    return -1;
-  }
-
-  // A header timestamp MUST be less than or equal to network-adjusted time plus
-  // 2 hours.
-  if (header.time > now + std::chrono::hours(2)) {
-    return -1;
-  }
-
-  // A header's version number MUST NOT have been retired by any activated
-  // soft fork.
-  // TODO
-
-  return -1;
+  auto state = BlockValidationState{};
+  bool const result = ContextualCheckBlockHeader(
+    legacy::convert_header(header), state, legacy::convert_consensus(*_params),
+    AnyChainView{chain | std::views::transform(legacy::convert_header)},
+    NodeClock::time_point{now.time_since_epoch()});
+  return result ? 0 : -1; // state.GetResult();
 }
 
 } // namespace bitcoin
